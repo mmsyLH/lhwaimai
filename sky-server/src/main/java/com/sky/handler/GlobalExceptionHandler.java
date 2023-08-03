3d1f30fd53ee61570ca6c4ff21cab8085826e247
lh <1072344372@qq.com>
2023-08-03 17:03:20 +0800

    新增员工代码开发

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
+ *
+ * @author 罗汉
+ * @date 2023/08/03
  */
 @RestControllerAdvice
 @Slf4j
@@ -24,4 +30,24 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 账户已存在异常处理
+     *
+     * @param ex 前女友
+     * @return {@link Result}
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //Duplicate entry 'luohan' for key 'idx_username'
+        String message = ex.getMessage();
+        if (message.contains("Duplicate entry")){
+            //希望给前端一个提示 动态提示用户名已经存在
+            String[] split = message.split(" ");
+            String username = split[2];
+            String msg=username+ MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
