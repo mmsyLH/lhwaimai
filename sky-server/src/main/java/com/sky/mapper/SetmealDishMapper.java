4b5f5e0e1985e861bc9c2ca2f5897f65988c7166
lh <1072344372@qq.com>
2023-08-08 01:21:08 +0800

    删除菜品业务代码功能代码实现

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,8 +1,21 @@
 package com.sky.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
 /**
  * @author :罗汉
  * @date : 2023/8/8
  */
+@Mapper
 public interface SetmealDishMapper {
+    /**
+     * 据菜品Id查询菜单id
+     *
+     * @param dishIds 菜id
+     * @return {@link List}<{@link Long}>
+     */
+    //select setmeal id from setmeal dis where dish_id in(1,2,3,4,5)
+    List<Long> getSetmealIdsByDishIds(List<Long>dishIds);
 }
