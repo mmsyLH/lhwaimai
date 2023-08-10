6a356ec908a2a7a1099cad2bca8d5b6958e2dcf7
lh <1072344372@qq.com>
2023-08-10 14:39:13 +0800

    套餐查询功能代码实现

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -1,8 +1,11 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
 import com.sky.vo.SetmealVO;
@@ -10,10 +13,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 套餐管理相关接口
@@ -45,5 +45,19 @@ public class SetmealController {
         log.info("新增套餐：{}",setmealDTO );
         setmealService.save(setmealDTO);
         return Result.success();
+    }
+    /**
+     * 套餐分页查询
+     *
+     * @param setmealPageQueryDTO 套餐分页查询dto
+     * @return {@link Result}<{@link PageResult}>
+     */
+    @GetMapping("/page")
+    @ApiOperation(value = "套餐分页查询")//
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO){//springMVC会自动封装好
+        log.info("套餐分页查询参数：{}",setmealPageQueryDTO );
+
+        PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
+        return  Result.success(pageResult);
     }
 }
