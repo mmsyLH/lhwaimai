d73b5025a71a90c1b4ed3caaee7bcbfcbc43a270
lh <1072344372@qq.com>
2023-08-13 02:20:44 +0800

    使用redis设置店铺营业状态、查询营业状态功能代码实现

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,29 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * redis配置
+ *
+ * @author 罗汉
+ * @date 2023/08/13
+ */
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        log.info("开始创建redis模板对象");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        //设置redis的连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置redis key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,52 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author :罗汉
+ * @date : 2023/8/13
+ */
+@RestController("adminShopController")//@RestController 是一个 Spring MVC 注解，它的作用是将一个类标记为 RESTful Web 服务的控制器。
+// 与传统的 Spring MVC控制器不同，@RestController 控制器的每个方法都会将返回值
+// 直接转换为 JSON 或 XML 格式的响应体，而无需使用视图解析器进行视图渲染。
+@RequestMapping("/admin/shop")
+@Slf4j
+@Api(tags = "店铺相关接口")//描述当前类的作用
+public class ShopController {
+    private static final String KEY="SHOP_STATUS";
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 设置店铺营业状态
+     *
+     * @param status 状态
+     * @return {@link Result}
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺营业状态")
+    private Result setStatus(@PathVariable Integer status) {
+        log.info("设置店铺营业状态为:{}", status==1?"营业中":"打样中");
+        redisTemplate.opsForValue().set(KEY, status);
+        return Result.success();
+    }
+
+    /**
+     * 获取店铺营业状态
+     *
+     * @return {@link Result}<{@link Integer}>
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺营业状态")
+    public Result<Integer> getStatus() {
+        Integer shop_status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取店铺营业状态为：{}",shop_status==1?"营业中":"打样中");
+        return Result.success(shop_status);
+    }
+}
