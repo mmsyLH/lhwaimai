07bbc1e9befbb36e72d28220d509a59efa346df4
lh <1072344372@qq.com>
2023-08-22 14:45:05 +0800

    redis缓存套餐代码功能代码实现

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,6 +38,7 @@ public class SetmealController {
      */
     @PostMapping("")
     @ApiOperation(value = "新增套餐")//用在方法上的描述注解
+    @CacheEvict(cacheNames = "setmealCache",key ="#setmealDTO.categoryId")//key : setmealCache::100
     public Result<String> save(@RequestBody SetmealDTO setmealDTO) {
 
         System.out.println("当前线程id："+Thread.currentThread().getId());
@@ -61,6 +63,7 @@ public class SetmealController {
     }
     @PostMapping("status/{status}")
     @ApiOperation(value = "起售停售套餐")//
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result startOrStop(@PathVariable Integer status,Long id){
         // public Result startOtStop(@PathVariable("status") Integer status,Long id){//变量同名可以省略
         log.info("起售停售套餐:{},{}",status,id);
@@ -70,6 +73,7 @@ public class SetmealController {
 
     @DeleteMapping
     @ApiOperation("批量删除套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids){
         log.info("批量删除套餐：{}",ids);
         setmealService.delete(ids);
@@ -96,6 +100,7 @@ public class SetmealController {
      */
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO) {
         setmealService.update(setmealDTO);
         return Result.success();
