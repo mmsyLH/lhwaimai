92b854e9cf2eb43c621c0907872ddc93ab58670b
lh <1072344372@qq.com>
2023-08-26 20:05:57 +0800

    c端历史订单查询功能代码实现

--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 import com.sky.entity.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -19,4 +20,11 @@ public interface OrderDetailMapper {
      * @param orderDetails 订单细节
      */
     void insertBatch(List<OrderDetail> orderDetails);
+    /**
+     * 根据订单id查询订单明细
+     * @param orderId
+     * @return
+     */
+    @Select("select * from order_detail where order_id = #{orderId}")
+    List<OrderDetail> getByOrderId(Long orderId);
 }
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,8 +1,11 @@
 package com.sky.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.entity.*;
@@ -11,10 +14,12 @@ import com.sky.exception.OrderBusinessException;
 import com.sky.exception.ShoppingCartBusinessException;
 import com.sky.mapper.*;
 import com.sky.properties.WeChatProperties;
+import com.sky.result.PageResult;
 import com.sky.service.OrderService;
 import com.sky.utils.WeChatPayUtil;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -69,14 +74,14 @@ public class OrderServiceImpl implements OrderService {
 
         // 2 像订单表插入一条数据
         Orders orders = new Orders();
-        BeanUtils.copyProperties(ordersSubmitDTO,orders);
+        BeanUtils.copyProperties(ordersSubmitDTO, orders);
 
         orders.setOrderTime(LocalDateTime.now());
         orders.setPayStatus(Orders.UN_PAID);
-        orders.setStatus(Orders.PAID);//待付款
-        orders.setNumber(String.valueOf(System.currentTimeMillis())+userId);
+        orders.setStatus(Orders.PAID);// 待付款
+        orders.setNumber(String.valueOf(System.currentTimeMillis()) + userId);
         orders.setPhone(addressBook.getPhone());
-        orders.setConsignee(addressBook.getConsignee());//收件人
+        orders.setConsignee(addressBook.getConsignee());// 收件人
         orders.setUserId(userId);
         orderMapper.insert(orders);
 
@@ -84,8 +89,8 @@ public class OrderServiceImpl implements OrderService {
         // 3 向订单明细表插入N条数据
         for (ShoppingCart cart : shoppingCartList) {
             OrderDetail orderDetail = new OrderDetail();
-            BeanUtils.copyProperties(cart,orderDetail);
-            orderDetail.setOrderId(orders.getId());//在mapper离设置好了的  关联的订单Id 在order表中
+            BeanUtils.copyProperties(cart, orderDetail);
+            orderDetail.setOrderId(orders.getId());// 在mapper离设置好了的  关联的订单Id 在order表中
             orderDetails.add(orderDetail);
         }
         orderDetailMapper.insertBatch(orderDetails);
@@ -101,6 +106,7 @@ public class OrderServiceImpl implements OrderService {
                 .build();
         return orderSubmitVO;
     }
+
     /**
      * 订单支付
      *
@@ -112,12 +118,12 @@ public class OrderServiceImpl implements OrderService {
         Long userId = BaseContext.getCurrentId();
         User user = userMapper.getById(userId);
 
-        //调用微信支付接口，生成预支付交易单
+        // 调用微信支付接口，生成预支付交易单
         JSONObject jsonObject = weChatPayUtil.pay(
-                ordersPaymentDTO.getOrderNumber(), //商户订单号
-                new BigDecimal(0.01), //支付金额，单位 元
-                "苍穹外卖订单", //商品描述
-                user.getOpenid() //微信用户的openid
+                ordersPaymentDTO.getOrderNumber(), // 商户订单号
+                new BigDecimal(0.01), // 支付金额，单位 元
+                "苍穹外卖订单", // 商品描述
+                user.getOpenid() // 微信用户的openid
         );
 
         if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
@@ -149,5 +155,46 @@ public class OrderServiceImpl implements OrderService {
                 .build();
 
         orderMapper.update(orders);
+    }
+
+    /**
+     * 历史订单分页查询
+     *
+     * @param pageNum  页面
+     * @param pageSize 页面大小
+     * @param status   状态
+     * @return {@link PageResult}
+     */
+    @Override
+    public PageResult page(int pageNum, int pageSize, Integer status) {
+        // 设置分页
+        PageHelper.startPage(pageNum, pageSize);
+
+        OrdersPageQueryDTO ordersPageQueryDTO = new OrdersPageQueryDTO();
+        ordersPageQueryDTO.setUserId(BaseContext.getCurrentId());
+        ordersPageQueryDTO.setStatus(status);
+
+        // 分页查询条件
+        Page<Orders> page = orderMapper.pageQuery(ordersPageQueryDTO);
+
+        List<OrderVO> list = new ArrayList<>();
+
+        // 查询出订单明细，并封装入OrderVO进行响应
+        if (page != null && page.getTotal() > 0) {
+            for (Orders orders : page) {
+                Long orderId = orders.getId();//订单id
+
+                //查询订单明细
+                List<OrderDetail> orderDetails=orderDetailMapper.getByOrderId(orderId);
+
+                OrderVO orderVO = new OrderVO();
+                BeanUtils.copyProperties(orders,orderVO);
+                orderVO.setOrderDetailList(orderDetails);
+
+                list.add(orderVO);
+
+            }
+        }
+        return new PageResult(page.getTotal(),list);
     }
 }
