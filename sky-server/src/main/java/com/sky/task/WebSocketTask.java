74003290000b02c40e9cc729c621fe1d99d47f56
lh <1072344372@qq.com>
2023-08-28 19:32:42 +0800

    来单提醒功能代码实现

--- sky-server/src/main/java/com/sky/task/WebSocketTask.java
+++ sky-server/src/main/java/com/sky/task/WebSocketTask.java
@@ -17,6 +17,6 @@ public class WebSocketTask {
      */
     @Scheduled(cron = "0/5 * * * * ?")
     public void sendMessageToClient() {
-        webSocketServer.sendToAllClient("这是来自服务端的消息：" + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
+        // webSocketServer.sendToAllClient("这是来自服务端的消息：" + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
     }
 }
